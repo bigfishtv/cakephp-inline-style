01b5ef3551e78bf52fc97361fbdaaa782e822387
Jacob Clarke <jacob@bigfish.tv>
2019-07-25 17:16:02 +1000

    Updated to new css inlining library: Emogrifier

--- src/InlineStyleHelper.php
+++ src/InlineStyleHelper.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\View\Helper;
 
 class InlineStyleHelper extends \Cake\View\Helper
@@ -6,12 +7,33 @@ class InlineStyleHelper extends \Cake\View\Helper
     public function afterLayout(\Cake\Event\Event $event)
     {
         $content = $event->subject->Blocks->get('content');
-        $parser = new \InlineStyle\InlineStyle();
-        libxml_use_internal_errors(true);
-        $parser->loadHTML($content);
-        libxml_use_internal_errors(false);
-        $parser->applyStylesheet($parser->extractStylesheets());
-        $content = $parser->getHtml();
+
+        // replace stylesheet links with style tags
+        $content = preg_replace_callback('/<link (.+?)>/', function ($matches) {
+            if (preg_match('/href="(.+?)"/', $matches[1], $matches2)) {
+                return '<style type="text/css">' . file_get_contents($matches2[1]) . '</style>';
+            }
+            return '';
+        }, $content);
+
+        // extract css from style tags
+        $css = '';
+        if (preg_match_all('/<style[^>]*>(.+?)<\/style>/is', $content, $matches, PREG_SET_ORDER)) {
+            foreach ($matches as $style) {
+                $css .= $style[1];
+            }
+        }
+
+        // strip css comments
+        $content = preg_replace('#/\*(.+?)\*/#s', '', $content);
+
+        // apply inline styles to css
+        $emogrifier = new \Pelago\Emogrifier($content, $css);
+        $emogrifier->disableInvisibleNodeRemoval();
+        $emogrifier->disableStyleBlocksParsing();
+
+        // set new html content 
+        $content = $emogrifier->emogrify();
         $event->subject->Blocks->set('content', $content);
     }
 }
